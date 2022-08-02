d272193996af5d41ccafc68c477feee31f21870d
keisuke mori <keisuke.forefore@gmail.com>
2022-08-02 19:41:13 +0900

    220802 UpDate

--- src/index.js
+++ src/index.js
@@ -9,15 +9,45 @@ const onClickAdd = () => {
   const div = document.createElement("div");
   div.classList.add("listRow");
   //div.className = "listRow";上のやつと同じ意味
-  console.log(div);
 
   //liタグを生成
   const li = document.createElement("li");
   li.innerText = inputText;
-  console.log(li);
+
+  //完了buttonを生成。
+  const completeButton = document.createElement("button");
+
+  completeButton.innerText = "完了";
+  const returnButton = document.createElement("Button");
+  completeButton.addEventListener("click", () => {
+    //完了ボタンが押されたら、ボタンの親要素のdivごと削除&完了リストに追加。
+    const completeTarget = completeButton.parentNode;
+    returnButton.innerText = "戻す";
+    document.getElementById("completeList").appendChild(li);
+    document.getElementById("completeList").appendChild(returnButton);
+    document.getElementById("incompleteList").removeChild(completeTarget); //配下のものを削除。
+  });
+
+  returnButton.addEventListener("click", () => {
+    const returnTarget = returnButton.parentNode;
+    document.getElementById("incompleteList").appendChild(li);
+    document.getElementById("incompleteList").appendChild(completeButton);
+    document.getElementById("incompleteList").appendChild(deleteButton);
+  });
+
+  //削除buttonを生成。
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //削除ボタンが押されたら、ボタンの親要素のdivごと削除する処理
+    const deleteTarget = deleteButton.parentNode; //親要素を指定。
+    document.getElementById("incompleteList").removeChild(deleteTarget); //配下のものを削除。
+  });
 
   //divの下にliとかの子要素を追加。
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   console.log(div);
 
   //未完了の要素に追加。
